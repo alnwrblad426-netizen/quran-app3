0ab9d200cea95801aef446ee78069b12be09bf10
alnwrblad426-netizen <alnwrblad426@gmail.com>
2026-07-24 23:01:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.label import Label
 
 class QuranApp(App):
     def build(self):
-        return Label(text='مرحباً بك في تطبيق القرآن الكريم', font_size=24)
+        return Label(text='تطبيق القرآن الكريم', font_name='Cairo-VariableFont_slnt,wght.ttf')
 
 if __name__ == '__main__':
     QuranApp().run()
