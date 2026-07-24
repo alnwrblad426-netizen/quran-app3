8ef9a64d2cfa81f7a3ce02b91064ef51c69c2e7b
alnwrblad426-netizen <alnwrblad426@gmail.com>
2026-07-24 23:40:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from kivy.app import App
 from kivy.uix.label import Label
+import arabic_reshaper
+from bidi.algorithm import get_display
 
 class QuranApp(App):
     def build(self):
-        return Label(text='تطبيق القرآن الكريم', font_name='Cairo-VariableFont_slnt,wght.ttf')
+        text = "تطبيق القرآن الكريم"
+        reshaped_text = arabic_reshaper.reshape(text)
+        bidi_text = get_display(reshaped_text)
+        return Label(text=bidi_text, font_name='Cairo-VariableFont_slnt,wght.ttf')
 
 if __name__ == '__main__':
     QuranApp().run()
